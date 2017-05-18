5797795c4913317a3eec516b38c35d73f43bc0f9
Jos Kuijpers <jkuijpers@jarvix.nl>
2017-05-18 15:20:15 +0200

    Update latitude configuration

--- FS17_RM_GEO_Wales/src/loader.lua
+++ FS17_RM_GEO_Wales/src/loader.lua
@@ -9,20 +9,8 @@
 
 local geoMod = {}
 geoMod.modDir = g_currentModDirectory
-geoMod.latitude = 65.4
 
 function g_rm_seasons_load(seasons)
     -- Load data from data/ folder
     g_seasons:registerXMLDirectory("northernsweden", geoMod.modDir .. "data/")
 end
-
-function geoMod.daylightLoad(daylight)
-    daylight.latitude = geoMod.latitude
-end
-
-function geoMod:loadMap()
-    -- Overwrite load function to replace latitude in any savegame
-    g_seasons.daylight.load = Utils.appendedFunction(g_seasons.daylight.load, geoMod.daylightLoad)
-end
-
-FSBaseMission.loadMap = Utils.prependedFunction(FSBaseMission.loadMap, geoMod.loadMap)
