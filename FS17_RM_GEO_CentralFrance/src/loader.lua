bb0a9fe8093356b447dafa49c7f324ef41cee71d
reallogger <fsgamelogger@gmail.com>
2017-06-29 09:25:52 +0200

    Update loader.lua

--- FS17_RM_GEO_CentralFrance/src/loader.lua
+++ FS17_RM_GEO_CentralFrance/src/loader.lua
@@ -12,22 +12,5 @@ geoMod.modDir = g_currentModDirectory
 
 function g_rm_seasons_load(seasons)
     -- Load data from data/ folder
-    g_seasons:registerXMLDirectory("snowylands", geoMod.modDir .. "data/")
+    g_seasons:registerXMLDirectory("midwest", geoMod.modDir .. "data/")
 end
-
-function geoMod:setup()
-    g_seasons.environment.calculateVisualSeason = Utils.overwrittenFunction(g_seasons.environment.calculateVisualSeason, geoMod.calculateVisualSeason)
-
-    -- Run the visual season listeners
-    g_seasons.environment:callListeners()
-
-    g_seasons.snow.mode = g_seasons.snow.MODE_ON
-end
-
-function geoMod:calculateVisualSeason(superFunc)
-    superFunc(self)
-
-    return g_seasons.environment.SEASON_WINTER
-end
-
-Mission00.loadMission00Finished = Utils.appendedFunction(Mission00.loadMission00Finished, geoMod.setup)
